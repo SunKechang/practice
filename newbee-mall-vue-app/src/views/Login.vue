77f8546d65b082241dfcebdcd285bae59df58417
li-02 <2272944319@qq.com>
2023-01-05 14:06:28 +0800

    登陆页面，还没写完

--- newbee-mall-vue-app/src/views/Login.vue
+++ newbee-mall-vue-app/src/views/Login.vue
@@ -1,43 +1,47 @@
-<!--
- * 严肃声明：
- * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
- * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
- * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
- * Copyright (c) 2020 陈尼克 all rights reserved.
- * 版权所有，侵权必究！
- *
--->
-
 <template>
   <div class="login">
-    <s-header :name="type == 'login' ? '登录' : '注册'" :back="'/home'"></s-header>
-    <img class="logo" src="//s.weituibao.com/1582958061265/mlogo.png" alt="">
-    <div v-if="type == 'login'" class="login-body login">
-      <van-form @submit="onSubmit">
-        <van-field
-          v-model="username"
-          name="username"
-          label="用户名"
-          placeholder="用户名"
-          :rules="[{ required: true, message: '请填写用户名' }]"
-        />
-        <van-field
-          v-model="password"
-          type="password"
-          name="password"
-          label="密码"
-          placeholder="密码"
-          :rules="[{ required: true, message: '请填写密码' }]"
-        />
-        <div class="verify">
-          <Verify ref="loginVerifyRef" @error="error" :showButton="false" @success="success" :width="'100%'" :height="'40px'" :fontSize="'16px'" :type="2"></Verify>
-        </div>
-        <div style="margin: 16px;">
-          <div class="link-register" @click="toggle('register')">立即注册</div>
-          <van-button round block type="info" color="#1baeae" native-type="submit">登录</van-button>
-        </div>
-      </van-form>
+    <!--    <s-header :name="ruleForm.type === 'login' ? '登录' : '注册'" :back="'/home'"></s-header>-->
+    <div v-if="ruleForm.type === 'login'" class="login-body login">
+      <!--      <van-form @submit="onSubmit">-->
+      <!--        <van-field-->
+      <!--          v-model="username"-->
+      <!--          name="username"-->
+      <!--          label="用户名"-->
+      <!--          placeholder="用户名"-->
+      <!--          :rules="[{ required: true, message: '请填写用户名' }]"-->
+      <!--        />-->
+      <!--        <van-field-->
+      <!--          v-model="password"-->
+      <!--          type="password"-->
+      <!--          name="password"-->
+      <!--          label="密码"-->
+      <!--          placeholder="密码"-->
+      <!--          :rules="[{ required: true, message: '请填写密码' }]"-->
+      <!--        />-->
+      <!--        <div class="verify">-->
+      <!--          <Verify ref="loginVerifyRef" @error="error" :showButton="false" @success="success" :width="'100%'" :height="'40px'" :fontSize="'16px'" :type="2"></Verify>-->
+      <!--        </div>-->
+      <!--        <div style="margin: 16px;">-->
+      <!--          <div class="link-register" @click="toggle('register')">立即注册</div>-->
+      <!--          <van-button round block type="info" color="#1baeae" native-type="submit">登录</van-button>-->
+      <!--        </div>-->
+      <!--      </van-form>-->
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="text" @click="toRegister()">注册</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+
     <div v-else class="login-body register">
       <van-form @submit="onSubmit">
         <van-field
@@ -76,26 +80,43 @@ import Verify from 'vue2-verify'
 export default {
   data() {
     return {
-      username: '',
-      password: '',
-      username1: '',
-      password1: '',
-      type: 'login',
-      verify: false
+      ruleForm: {
+        username: '',
+        password: '',
+        username1: '',
+        password1: '',
+        type: 'login',
+        verify: false
+      },
+      rules:
+        {
+          username: [
+            { required:true,message:'请输入用户名',trigger: 'blur'}
+          ],
+          password: [
+            { required:true,message:'请输入密码', trigger: 'blur'}
+          ]
+        }
     }
   },
-  components: {
-    sHeader,
-    Verify
-  },
+  // components: {
+  //   sHeader,
+  //   Verify
+  // },
   methods: {
-    dealTriVer() {
-      // 执行验证码的验证，通过 this.verify 知道验证码是否填写正确
-      this.$refs.loginVerifyRef.$refs.instance.checkCode()
-    },
+    // dealTriVer() {
+    //   // 执行验证码的验证，通过 this.verify 知道验证码是否填写正确
+    //   this.$refs.loginVerifyRef.$refs.instance.checkCode()
+    // },
     toggle(v) {
       this.verify = false
       this.type = v
+    },
+    submitForm(){
+
+    },
+    toRegister(){
+      this.ruleForm.type='register'
     },
     async onSubmit(values) {
       this.dealTriVer()
@@ -103,7 +124,7 @@ export default {
         Toast.fail('验证码未填或填写错误!')
         return
       }
-      if (this.type == 'login') {
+      if (this.type === 'login') {
         const { data, resultCode } = await login({
           "loginName": values.username,
           "passwordMd5": this.$md5(values.password)
@@ -134,70 +155,68 @@ export default {
 </script>
 
 <style lang="less">
+.login {
+  margin: auto;
+  width: 400px;
+  height: 200px;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
   .login {
-    .logo {
-      width: 120px;
-      height: 120px;
-      display: block;
-      margin: 80px auto 0px;
+    .link-register {
+      font-size: 14px;
+      margin-bottom: 20px;
+      color: #1989fa;
+      display: inline-block;
     }
-    .login-body {
-      padding: 0 20px;
+  }
+  .register {
+    .link-login {
+      font-size: 14px;
+      margin-bottom: 20px;
+      color: #1989fa;
+      display: inline-block;
     }
-    .login {
-      .link-register {
-        font-size: 14px;
-        margin-bottom: 20px;
-        color: #1989fa;
-        display: inline-block;
-      }
+  }
+  .verify-bar-area {
+    margin-top: 24px;
+    .verify-left-bar {
+      border-color: #1baeae;
     }
-    .register {
-      .link-login {
-        font-size: 14px;
-        margin-bottom: 20px;
-        color: #1989fa;
-        display: inline-block;
-      }
+    .verify-move-block {
+      background-color: #1baeae;
+      color: #fff;
     }
-    .verify-bar-area {
-      margin-top: 24px;
-      .verify-left-bar {
-        border-color: #1baeae;
-      }
-      .verify-move-block {
-        background-color: #1baeae;
-        color: #fff;
-      }
+  }
+  .verify {
+    >div {
+      width: 100%;
     }
-    .verify {
-      >div {
-        width: 100%;
+    display: flex;
+    justify-content: center;
+    .cerify-code-panel {
+      margin-top: 16px;
+    }
+    .verify-code {
+      width: 40%!important;
+      float: left!important;
+    }
+    .verify-code-area {
+      float: left!important;
+      width: 54%!important;
+      margin-left: 14px!important;
+      .varify-input-code {
+        width: 90px;
+        height: 38px!important;
+        border: 1px solid #e9e9e9;
+        padding-left: 10px;
+        font-size: 16px;
       }
-      display: flex;
-      justify-content: center;
-      .cerify-code-panel {
-        margin-top: 16px;
-      }
-      .verify-code {
-        width: 40%!important;
-        float: left!important;
-      }
-      .verify-code-area {
-        float: left!important;
-        width: 54%!important;
-        margin-left: 14px!important;
-        .varify-input-code {
-          width: 90px;
-          height: 38px!important;
-          border: 1px solid #e9e9e9;
-          padding-left: 10px;
-          font-size: 16px;
-        }
-        .verify-change-area {
-          line-height: 44px;
-        }
+      .verify-change-area {
+        line-height: 44px;
       }
     }
   }
+}
 </style>
