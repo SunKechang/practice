c689767ee871c63c9d934f73653288e467706ca1
li-02 <2272944319@qq.com>
2023-01-05 20:51:16 +0800

    登录和注册

--- newbee-mall-vue-app/src/views/User.vue
+++ newbee-mall-vue-app/src/views/User.vue
@@ -1,19 +1,12 @@
-<!--
- * 严肃声明：
- * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
- * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
- * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
- * Copyright (c) 2020 陈尼克 all rights reserved.
- * 版权所有，侵权必究！
- *
--->
-
 <template>
   <div class="user-box">
-    <s-header :name="'我的'"></s-header>
+<!--    <s-header :name="'我的'"></s-header>-->
+
+    <TopNavigator>
+    </TopNavigator>
     <div class="user-info">
       <div class="info">
-        <img src="//s.weituibao.com/1583583975067/user-graduate%20(1).png"/>
+        <img src="../assets/takina.jpg"/>
         <div class="user-desc">
           <span>昵称：{{ user.nickName }}</span>
           <span>登录名：{{ user.loginName }}</span>
@@ -47,10 +40,12 @@
 import navBar from '@/components/NavBar'
 import sHeader from '@/components/SimpleHeader'
 import { getUserInfo } from '../service/user'
+import TopNavigator from '@/components/TopNavigator'
 export default {
   components: {
     navBar,
-    sHeader
+    // sHeader
+    TopNavigator
   },
   data() {
     return {
@@ -75,23 +70,6 @@ export default {
 <style lang="less" scoped>
   @import '../common/style/mixin';
   .user-box {
-    .user-header {
-      position: fixed;
-      top: 0;
-      left: 0;
-      z-index: 10000;
-      .fj();
-      .wh(100%, 44px);
-      line-height: 44px;
-      padding: 0 10px;
-      .boxSizing();
-      color: #252525;
-      background: #fff;
-      border-bottom: 1px solid #dcdcdc;
-      .user-name {
-        font-size: 14px;
-      }
-    }
     .user-info {
       width: 94%;
       margin: 10px;
